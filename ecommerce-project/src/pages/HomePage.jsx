cea0ac77b10222d6d8c4eb09ef91820d3b7d1a1d
walid <waliddrici35@gmail.com>
2025-08-31 18:53:12 +0100

    use axios the fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
+import axios from 'axios'
 import CheckMark from '../assets/images/icons/checkmark.png'
 import Header from "../components/Header"
 import { products } from '../../starting-code/data/products'
 import "./HomePage.css"
 
 function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
   .then((response) => {
-    return response.json()
-    }).then((data)=>{
-      console.log(data)
+    response.data
   })
 
   return (
