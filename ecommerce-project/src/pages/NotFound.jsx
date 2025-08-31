97593e5e2885ef3a287fad1d11817e3521968a16
walid <waliddrici35@gmail.com>
2025-08-31 11:20:58 +0100

    Added Notfound page

---
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import './NotFound.css'
+function NotFound(){
+  return(
+    <>
+      <title>Not Found</title>
+      <Header />
+      <p className='Not-Found-text'>Error 404 Not Found</p>
+    </>
+  )
+}
+
+export default NotFound
