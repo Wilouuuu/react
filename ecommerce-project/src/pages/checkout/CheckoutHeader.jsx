fe22a83b6690096296ca2be839ae2c5d6eab43bc
walid <waliddrici35@gmail.com>
2025-09-04 14:48:02 +0100

    passing cart into checkout header and displaying the total quantity of items

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -4,27 +4,33 @@ import CheckLock from '../../assets/images/icons/checkout-lock-icon.png'
 import { Link } from 'react-router'
 import './CheckoutHeader.css'
 
-function CheckoutHeader(){
-  return(
+function CheckoutHeader({ cart }) {
+  let totalQuantity = 0;
+  
+  cart.forEach((product) => {
+    totalQuantity += product.quantity
+  })
+
+  return (
     <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/">
-              <img className="logo" src={Logo} />
-              <img className="mobile-logo" src={MobileLogo} />
-            </Link>
-          </div>
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src={Logo} />
+            <img className="mobile-logo" src={MobileLogo} />
+          </Link>
+        </div>
 
-          <div className="checkout-header-middle-section">
-            Checkout (<Link className="return-to-home-link"
-              to="/">3 items</Link>)
-          </div>
+        <div className="checkout-header-middle-section">
+          Checkout (<Link className="return-to-home-link"
+            to="/">{totalQuantity} items</Link>)
+        </div>
 
-          <div className="checkout-header-right-section">
-            <img src={CheckLock} />
-          </div>
+        <div className="checkout-header-right-section">
+          <img src={CheckLock} />
         </div>
       </div>
+    </div>
 
   )
 }
